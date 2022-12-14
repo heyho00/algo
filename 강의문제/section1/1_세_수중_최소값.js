71b06f113468416457102569b768c37374988f39
성해호 <hhseong@tidesquare.com>
2022-12-14 11:26:54 +0900

    min값 구하기

---
+++ 강의문제/section1/1_세_수중_최소값.js
@@ -0,0 +1,9 @@
+
+// "100이하의 자연수 A, B, C를 입력받아 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하 세요.(정렬을 사용하면 안됩니다) ";
+
+      function solution(a, b, c) {
+        return Math.min(a,b,c)
+      }
+
+
+      console.log(solution(10,2,3))
