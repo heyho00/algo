ee7c2f0772a90b81d06b32f3440ee528f231ed1e
성해호 <hhseong@tidesquare.com>
2023-02-17 10:09:30 +0900

    programmers

--- programmers/0.js
+++ programmers/0.js
@@ -1,20 +1,17 @@
 // 연습장 
 // node 0.js
 
-function solution(n) {
-    let dp = new Array(n+1).fill(1)
-    // console.log(dp)
-    for(let i = 2 ; i <= n ; i++){
-        if(dp[i]){
-            for(let j = 2 ; i*j <= n ; j++){
-                dp[i*j] = 0
-            }
-        }
-    }
+function solution(nums, direc) {
+    numbers = [...nums, ...nums]
+    let leng = nums.length
 
-    return dp.filter(el => el === 0).length
+    if (direc === 'right') {
+        return numbers.splice(leng - 1, leng)
+    }
+    return numbers.splice(1, leng)
 }
 
-console.log(solution(15))
-// console.log(solution(7)) 
+
+console.log(solution([1, 2, 3], 'right'))
+console.log(solution([4, 455, 6, 4, -1, 45, 6], 'left'))
 
