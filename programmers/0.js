3dc0f9c1739b03ad579cd8bdcb4869648e24dca6
heyho00 <heyho9292@gmail.com>
2023-02-27 22:54:39 +0900

    programmers

--- programmers/0.js
+++ programmers/0.js
@@ -1,10 +1,16 @@
 // 연습장 
 // node 0.js
 
-function solution(n, k) {
-    const service = Math.floor(n/10)
-    let beverage = k-service >0 ? k-service : 0
-    return (12000 * n) + (2000*beverage)
+function solution(n) {
+  let cnt =1
+  let answer="\n"
+
+  while(cnt<=n){
+    answer += '*'.repeat(cnt) +'\n'
+    cnt++
+  }
+
+  return answer
 }
 
-console.log(solution(64, 6))
+console.log(solution(3)) 
