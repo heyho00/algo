e5d9c4006a2d980a5d3272b471b4504ec5baedc1
heyho00 <heyho9292@gmail.com>
2023-03-09 22:25:36 +0900

    programmers

--- programmers/0.js
+++ programmers/0.js
@@ -1,8 +1,14 @@
 // 연습장 
 // node 0.js
 
-function solution(str) {
- return str.split('').reverse().join('')
+function solution(price) {
+    let percent = 0
+
+    price >= 100000 && (percent = 5)
+    price >= 300000 && (percent = 10)
+    price >= 500000 && (percent = 20)
+
+    return Math.floor(price * (1-percent/100))
 }
 
-console.log(solution('jaron')) 
+console.log(solution(580000))
