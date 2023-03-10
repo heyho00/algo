8a40b391b87f3649e154c223e1ffc7512d192cfd
성해호 <hhseong@tidesquare.com>
2023-03-10 18:35:03 +0900

    programmers

--- programmers/0.js
+++ programmers/0.js
@@ -1,14 +1,8 @@
 // 연습장 
 // node 0.js
 
-function solution(price) {
-    let percent = 0
-
-    price >= 100000 && (percent = 5)
-    price >= 300000 && (percent = 10)
-    price >= 500000 && (percent = 20)
-
-    return Math.floor(price * (1-percent/100))
+function solution(n) {
+    return n % 7 === 0 ? (n / 7) : ~~(n / 7) + 1
 }
 
-console.log(solution(580000))
+console.log(solution(15))
