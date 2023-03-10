1c58915068714cdf22e17ea5909354ea512af6b0
heyho00 <heyho9292@gmail.com>
2023-03-10 23:19:41 +0900

    programmers

--- programmers/0.js
+++ programmers/0.js
@@ -1,8 +1,21 @@
-// 연습장 
+// 연습장
 // node 0.js
 
-function solution(n) {
-    return n % 7 === 0 ? (n / 7) : ~~(n / 7) + 1
+function solution(arr) {
+  let obj = {};
+  let answer = [];
+
+  arr.forEach((n) => {
+    obj[n] == undefined ? (obj[n] = 1) : (obj[n] += 1);
+  });
+
+  let value = Math.max(...Object.values(obj));
+
+  for (let x in obj) {
+    obj[x] === value && answer.push(x);
+  }
+
+  return answer.length > 1 ? -1 : Number(answer[0]);
 }
 
-console.log(solution(15))
+console.log(solution([1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 4]));
