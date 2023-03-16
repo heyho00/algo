e39f2cff6d12d28c4bb29092bf505a1d40da8a87
heyho00 <heyho9292@gmail.com>
2023-03-16 23:23:33 +0900

    programmers lv1 시작

--- programmers/0.js
+++ programmers/0.js
@@ -1,13 +1,40 @@
 // 연습장
 // node 0.js
 
-function solution(n1, d1, n2, d2) {
-  if (d2 % d1 === 0) {
-    n1 *= (d2 / d1)
-    d1 *= (d2 / d1)
+// 1. 요소 중 3개를 뽑아 만드는 부분배열의 경우의 수를 구한다
+// 2. 그 중 세 요소 더한게 0인 배열의 수를 리턴.
+
+function solution(left, right) {
+  let answer =0;
+
+  function getMeasure(n){
+    let cnt = 1
+    let arr =[]
+    while(n>=cnt){
+      n%cnt ===0 && (arr.push(cnt))
+      cnt++
+    }
+    return arr.length %2===0 ? n : -n
   }
-  console.log(n1, d1, 'sss')
-  return
+
+  for(let i=left; i<=right; i++){
+    answer+=getMeasure(i)
+  }
+  return answer
 }
 
-console.log(solution(1, 2, 3, 4));
+console.log(solution(13, 17));
+
+
+// 0,1,2
+// 0,1,3
+// 0,1,4
+
+// 0,2,3
+// 0,2,4
+// 0,3,4
+
+// 1,2,3
+// 1,2,4
+
+// 2,3,4
