bf925e776bbf938c338d246a9a3e41448abd14f3
heyho00 <heyho9292@gmail.com>
2023-03-17 19:52:30 +0900

    programmers

--- programmers/0.js
+++ programmers/0.js
@@ -4,37 +4,10 @@
 // 1. 요소 중 3개를 뽑아 만드는 부분배열의 경우의 수를 구한다
 // 2. 그 중 세 요소 더한게 0인 배열의 수를 리턴.
 
-function solution(left, right) {
-  let answer =0;
-
-  function getMeasure(n){
-    let cnt = 1
-    let arr =[]
-    while(n>=cnt){
-      n%cnt ===0 && (arr.push(cnt))
-      cnt++
-    }
-    return arr.length %2===0 ? n : -n
-  }
-
-  for(let i=left; i<=right; i++){
-    answer+=getMeasure(i)
-  }
-  return answer
+function solution(s) {
+  return s.length % 2 === 0 
+  ? s.split('').splice((s.length/2)-1, 2).join('')
+  : s.split('').splice(~~(s.length/2),1).join('')
 }
 
-console.log(solution(13, 17));
-
-
-// 0,1,2
-// 0,1,3
-// 0,1,4
-
-// 0,2,3
-// 0,2,4
-// 0,3,4
-
-// 1,2,3
-// 1,2,4
-
-// 2,3,4
+console.log(solution("abcde"));
